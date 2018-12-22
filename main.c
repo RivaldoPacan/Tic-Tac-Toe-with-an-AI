be432c81a1c657a9ece48ce7cad61dc5bd4bbe7f
unknown <varianto.rivaldo@gmail.com>
2018-12-22 13:18:03 +0700

    first commit

--- main.c
+++ main.c
@@ -1,283 +1,455 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <conio.h>
+#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 void board();
 void assign();
 int check();
 void computer();
-
+void moves();
+void printmoves();
 struct name
 {
-	char player1[20];
-	char player2[20];
+ char player1[20];
+ char player2[20];
+ int score1;
+ int score2;
 };
-char number[9] = {'1','2','3','4','5','6','7','8','9'};
-int buffer[9];
-int i;
-int main() 
+struct push
 {
-	struct name player;
-	int a,human = 1,input;
-	printf("\t=Hello There! Welcome to Rivaldo and Fashhandi Tic Tac Toe!!=\n\n");
-	printf("Player 1 name: ");
-	scanf("%s", &player.player1);
+ int data;
+ struct push *Ptr;
+};
+typedef struct push push;
+typedef push *nodePtr;
+int i;
+int main()
+{
+ nodePtr stackPtr = NULL;
+ struct name player;
+ char tempc;
+ int a=-1,human = 1,input = -1,play=0,b=0,c=0,d=-1, temp;
+ printf("\t=Hello There! Welcome to Rivaldo and Fashhandi Tic Tac Toe!!=\n\n");
+ printf("Player 1 name: ");
+ scanf("%s", &player.player1);
 
-	printf("Player 2 name: ");
-	scanf("%s", &player.player2);
-	int g=0;
-	int i;
-	system("cls");
-	char symbol;
-	do
-	{
-		board(number);
-		
-		human = (human % 2) ? 1 : 2;
-		symbol = (human == 1)? 'X' : 'O';
-		
-		if (human == 1)
-		{
-			printf("\n%s, Enter your number: ", player.player1);
-			scanf("%d", &input);	
-			
-			assign(input, symbol, number, human);
-		}
-		if (human == 2)
-		{
-			printf("\n%s, Enter your number: ", player.player2);
-			scanf("%d", &input);
-			if (input == 0)
-			{
-				computer(number);
-			}
-			assign(input, symbol, number, human);
-		}
-		a = check(number);
-		buffer[g] = input;
-		g++;
+ printf("Player 2 name: ");
+ scanf("%s", &player.player2);
+
+ int i;
+ system("cls");
+ char symbol, again;
+ do
+ {
+  int g=0;
+  char number[9] = {'1','2','3','4','5','6','7','8','9'};
+  int buffer[9] = {0,0,0,0,0,0,0,0,0};
+ do
+ {
+  board(number);
+
+  human = (human % 2) ? 1 : 2;
+  symbol = (human == 1)? 'X' : 'O';
+
+  if (human == 1)
+  {
+   printf("\n%s, Enter your number: ", player.player1);
+   temp = input;
+   scanf("%d", &input);
+    if (input == 0)
+   {
+    computer(number);
+   }
+
+   printf("%d", input);
+   while(input == temp)
+            {
+                printf("Your choice is invalid! Please enter a number between 1-9.");
+                scanf("%c%d", &tempc, &input);
+            }
+            while(input < 0 || input > 9)
+            {
+                printf("Your choice is invalid! Please enter a number between 1-9.");
+                scanf("%d", &input);
+            }
+   assign(input, symbol, number, human);
+   moves(&stackPtr, input);
+  }
+  if (human == 2)
+  {
+   printf("\n%s, Enter your number: ", player.player2);
+   temp = input;
+   scanf("%d", &input);
+
+
+   while(input == temp)
+            {
+                printf("Your choice is invalid! Please enter a number between 1-9.");
+                scanf("%c%d", &tempc, &input);
+            }
+            while(input < 0 || input > 9)
+            {
+                printf("Your choice is invalid! Please enter a number between 1-9.");
+                scanf("%d", &input);
+            }
+   assign(input, symbol, number, human);
+   moves(&stackPtr, input);
+  }
+  a = check(number);
+  buffer[g] = input;
+  g++;
         human++;
-		system("cls");
-	}while(a == -1);
-	board(number);
-	
-	if (a == 1)
-	{	
-		human--;
-		if(human == 1)
-		{
-			printf("\n--> %s WIN", player.player1);
-		}
-		if(human == 2)
-		{
-			printf("\n--> %s WIN", player.player2);
-		}
-	}
-	else
-	{
-		printf("\nGame is Draw");
-	}
-	return 0;
+  system("cls");
+ }while(a == -1);
+ board(number);
+
+ if (a == 1)
+ {
+  human--;
+  if(human == 1)
+  {
+   printf("\n--> %s WON\n", player.player1);
+   b++;
+   player.score1++;
+  }
+  else if(human == 2)
+  {
+   printf("\n--> %s WON\n", player.player2);
+   c++;
+   player.score2++;
+  }
+ }
+ else
+ {
+  printf("\nGame is Draw\n\n");
+ }
+ reverse(&stackPtr);
+ printmoves(stackPtr, player);
+ printf("Play again(Y/N)? ");
+ scanf(" %c", &again);
+ if (again == 'Y' || again == 'y')
+ {
+  d=-1;
+  system("cls");
+ }
+ if (again == 'N' || again == 'n')
+ {
+  d=1;
+  /*writeToFile(player.player1, player.player2, player.score1, player.score2);*/
+  break;
+ }
+ deletelist(&stackPtr);
+ }while(d==-1);
+
+ return 0;
 }
+
+/*void writeToFile(char *player1, char *player2, int score1, int score2)
+{
+    FILE *fp;
+    char str[50];
+    char temp;
+
+    fp = fopen("data.txt", "a+");
+    strcat(str, player1);
+    strcat(str, ";");
+    temp = score1 + '0';
+    strcat(str, temp);
+    strcat(str, ";");
+    strcat(str, player2);
+    strcat(str, ";");
+    itoa(score2, temp, 3);
+    strcat(str, temp);
+    strcat(str, ";");
+    strcat(str, "\n");
+
+    fprintf(fp, str);
+    fclose(fp);
+}*/
 
 void board(char * number)
 {
-	printf("\t\t\t\t\t=Tic Tac Toe=\n\n");
-	printf("    Player 1 (X)  -  Player 2 (O)\n\n\n");
-	printf("\t _____ _____ _____\n");
-	printf("\t|     |     |     |\n");
-	printf("\t|  %c  |  %c  |  %c  |\n", number[0], number[1], number[2]);
-	printf("\t|_____|_____|_____|\n");
-	printf("\t|     |     |     |\n");
-	printf("\t|  %c  |  %c  |  %c  |\n", number[3], number[4], number[5]);
-	printf("\t|_____|_____|_____|\n");
-	printf("\t|     |     |     |\n");
-	printf("\t|  %c  |  %c  |  %c  |\n", number[6], number[7], number[8]);
-	printf("\t|_____|_____|_____|\n"); 
+ printf("\t\t\t\t\t=Tic Tac Toe=\n\n");
+ printf("    Player 1 (X)  -  Player 2 (O)\n\n\n");
+ printf("\t _____ _____ _____\n");
+ printf("\t|     |     |     |\n");
+ printf("\t|  %c  |  %c  |  %c  |\n", number[0], number[1], number[2]);
+ printf("\t|_____|_____|_____|\n");
+ printf("\t|     |     |     |\n");
+ printf("\t|  %c  |  %c  |  %c  |\n", number[3], number[4], number[5]);
+ printf("\t|_____|_____|_____|\n");
+ printf("\t|     |     |     |\n");
+ printf("\t|  %c  |  %c  |  %c  |\n", number[6], number[7], number[8]);
+ printf("\t|_____|_____|_____|\n");
 }
 
 void assign(int input,int symbol,char * number,int human)
-{ 
-	
-	if (input == 1 && number[0] == '1')
+{
+
+    if (input == 1 && number[0] == '1')
     {
-    	number[0] = symbol;
-	}
+     number[0] = symbol;
+    }
     else if (input == 2 && number[1] == '2')
     {
-		number[1] = symbol;
-	}
+     number[1] = symbol;
+ }
     else if (input == 3 && number[2] == '3')
     {
-	    number[2] = symbol;
-	}
+     number[2] = symbol;
+ }
     else if (input == 4 && number[3] == '4')
     {
-	    number[3] = symbol;
-	}
+     number[3] = symbol;
+ }
     else if (input == 5 && number[4] == '5')
     {
-	    number[4] = symbol;
-	}
+     number[4] = symbol;
+ }
     else if (input == 6 && number[5] == '6')
     {
-	    number[5] = symbol;
-	}
+     number[5] = symbol;
+ }
     else if (input == 7 && number[6] == '7')
     {
-	    number[6] = symbol;
-	}
+     number[6] = symbol;
+ }
     else if (input == 8 && number[7] == '8')
     {
-	    number[7] = symbol;
-	}
+     number[7] = symbol;
+ }
     else if (input == 9 && number[8] == '9')
     {
-	    number[8] = symbol;
-	}
-    else if (input != 0)
-    {
-        printf("Invalid move ");
-        human--;
-        getch();
-    }
+     number[8] = symbol;
+ }
 }
 
 int check(char * number)
 {
     if (number[0] == number[1] && number[1] == number[2])
     {
-	    return 1;
-	}
+     return 1;
+ }
     else if (number[3] == number[4] && number[4] == number[5])
     {
-	    return 1;
-	}
+     return 1;
+ }
     else if (number[6] == number[7] && number[7] == number[8])
     {
-	    return 1;
-	}
+     return 1;
+ }
     else if (number[0] == number[3] && number[3] == number[6])
     {
-	    return 1;
-	}
+     return 1;
+ }
     else if (number[1] == number[4] && number[4] == number[7])
     {
-	    return 1;
-	}
+     return 1;
+ }
     else if (number[2] == number[5] && number[5] == number[8])
     {
-	    return 1;
-	}
+     return 1;
+ }
     else if (number[0] == number[4] && number[4] == number[8])
     {
-	    return 1;
-	}
+     return 1;
+ }
     else if (number[2] == number[4] && number[4] == number[6])
     {
-	    return 1;
-	}
+     return 1;
+ }
     if (number[0] != '1' && number[1] != '2' && number[2] != '3' &&
         number[3] != '4' && number[4] != '5' && number[5] != '6' && number[6]
         != '7' && number[7] != '8' && number[8] != '9')
-	{
+ {
         return 2;
-	}
+ }
     else
     {
-	    return  - 1;
-	}
+     return  - 1;
+ }
 }
 
 void computer(char * number)
 {
-    if(number[4] != 'X' && number[4] == '5')
+    if(number[4] != 'O' && number[4] == '5')
     {
-        number[4] = 'O';
+        number[4] = 'X';
     }
-    if(number[4] == 'X' && number[0] == '1')
+    else if(number[4] == 'O' && number[0] == '1')
     {
-        number[0] = 'O';
-    } 
-    else if(number[0] == 'X' && number[1] == 'X' && number[2] == '3')
-    {
-        number[2] = 'O';
+        number[0] = 'X';
     }
-    else if(number[2] == 'X' && number[1] == 'X' && number[0] == '1')
+    else if(number[0] == 'O' && number[1] == 'O' && number[2] == '3')
     {
-        number[0] = 'O';
+        number[2] = 'X';
     }
-    else if(number[0] == 'X' && number[2] == 'X' && number[1] == '2')
+    else if(number[2] == 'O' && number[1] == 'O' && number[0] == '1')
     {
-        number[1] = 'O';
+        number[0] = 'X';
     }
-    else if(number[3] == 'X' && number[4] == 'X' && number[5] == '6')
+    else if(number[0] == 'O' && number[2] == 'O' && number[1] == '2')
     {
-        number[5] = 'O';
+        number[1] = 'X';
     }
-    else if(number[5] == 'X' && number[4] == 'X' && number[3] == '4')
+    else if(number[3] == 'O' && number[4] == 'O' && number[5] == '6')
     {
-        number[3] = 'O';
+        number[5] = 'X';
     }
-    else if(number[3] == 'X' && number[5] == 'X' && number[4] == '5')
+    else if(number[5] == 'O' && number[4] == 'O' && number[3] == '4')
     {
-        number[4] = 'O';
+        number[3] = 'X';
     }
-    else if(number[6] == 'X' && number[7] == 'X' && number[8] == '9')
+    else if(number[3] == 'O' && number[5] == 'O' && number[4] == '5')
     {
-        number[8] = 'O';
+        number[4] = 'X';
     }
-    else if(number[8] == 'X' && number[7] == 'X' && number[6] == '7')
+    else if(number[6] == 'O' && number[7] == 'O' && number[8] == '9')
     {
-        number[6] = 'O';
+        number[8] = 'X';
     }
-    else if(number[6] == 'X' && number[8] == 'X' && number[7] == '8')
+    else if(number[8] == 'O' && number[7] == 'O' && number[6] == '7')
     {
-        number[7] = 'O';
+        number[6] = 'X';
     }
-    else if(number[0] == 'X' && number[3] == 'X' && number[6] == '7')
+    else if(number[6] == 'O' && number[8] == 'O' && number[7] == '8')
     {
-        number[6] = 'O';
+        number[7] = 'X';
     }
-    else if(number[6] == 'X' && number[3] == 'X' && number[0] == '1')
+    else if(number[0] == 'O' && number[3] == 'O' && number[6] == '7')
     {
-        number[0] = 'O';
+        number[6] = 'X';
     }
-    else if(number[0] == 'X' && number[6] == 'X' && number[3] == '4')
+    else if(number[6] == 'O' && number[3] == 'O' && number[0] == '1')
     {
-        number[3] = 'O';
+        number[0] = 'X';
     }
-    else if(number[1] == 'X' && number[4] == 'X' && number[7] == '8')
+    else if(number[0] == 'O' && number[6] == 'O' && number[3] == '4')
     {
-        number[7] = 'O';
+        number[3] = 'X';
     }
-    else if(number[7] == 'X' && number[4] == 'X' && number[1] == '2')
+    else if(number[1] == 'O' && number[4] == 'O' && number[7] == '8')
     {
-        number[1] = 'O';
+        number[7] = 'X';
     }
-    else if(number[1] == 'X' && number[7] == 'X' && number[4] == '5')
+    else if(number[7] == 'O' && number[4] == 'O' && number[1] == '2')
     {
-        number[4] = 'O';
+        number[1] = 'X';
     }
-    else if(number[2] == 'X' && number[5] == 'X' && number[8] == '9')
+    else if(number[1] == 'O' && number[7] == 'O' && number[4] == '5')
     {
-        number[8] = 'O';
+        number[4] = 'X';
     }
-    else if(number[8] == 'X' && number[5] == 'X' && number[2] == '3')
+    else if(number[2] == 'O' && number[5] == 'O' && number[8] == '9')
     {
-        number[2] = 'O';
+        number[8] = 'X';
     }
-    else if(number[2] == 'X' && number[8] == 'X' && number[5] == '6')
+    else if(number[8] == 'O' && number[5] == 'O' && number[2] == '3')
     {
-        number[5] = 'O';
+        number[2] = 'X';
     }
-    else if(number[6] == 'X' && number[4] == 'X' && number[2] == '3')
+    else if(number[2] == 'O' && number[8] == 'O' && number[5] == '6')
     {
-        number[2] = 'O';
+        number[5] = 'X';
     }
-    else if(number[2] == 'X' && number[4] == 'X' && number[6] == '7')
+    else if(number[6] == 'O' && number[4] == 'O' && number[2] == '3')
     {
-        number[6] = 'O';
+        number[2] = 'X';
     }
+    else if(number[2] == 'O' && number[4] == 'O' && number[6] == '7')
+    {
+        number[6] = 'X';
+    }
+    else if(number[2] == '3')
+    {
+        number[2] = 'X';
+    }
+    else if(number[6] == '7')
+    {
+        number[6] = 'X';
+    }
+    else if(number[8] == '9')
+    {
+        number[8] = 'X';
+    }
+    else if(number[1] == '2')
+    {
+        number[1] = 'X';
+    }
+    else if(number[3] == '4')
+    {
+        number[3] = 'X';
+    }
+    else if(number[4] == '5')
+    {
+        number[4] = 'X';
+    }
+    else if(number[5] == '6')
+    {
+        number[5] = 'X';
+    }
+    else if(number[7] == '8')
+    {
+        number[7] = 'X';
+    }
+}
+void moves(nodePtr *stackPtr,int input)
+{
+    nodePtr newPtr;
+    newPtr = malloc(sizeof(push));
+
+ if (newPtr != NULL)
+ {
+    newPtr->data = input;
+    newPtr->Ptr = *stackPtr;
+    *stackPtr = newPtr;
+ }
+}
+void printmoves(nodePtr currentPtr, struct name player)
+{
+ int x=1;
+ printf("Moves:\n");
+ while(currentPtr != NULL)
+ {
+  if (x%2!=0)
+  {
+   printf("%s\t\t: ", player.player1);
+  }
+  else if(x%2==0)
+  {
+   printf("%s\t\t: ", player.player2);
+  }
+  printf("Block %d\n", currentPtr->data);
+  currentPtr = currentPtr->Ptr;
+  x++;
+ }
+}
+void reverse(struct push** rev)
+{
+ struct push *prev = NULL;
+ struct push *current = *rev;
+ struct push *next = NULL;
+ while (current != NULL)
+ {
+  next = current->Ptr;
+  current->Ptr = prev;
+  prev = current;
+  current = next;
+ }
+ *rev = prev;
+}
+void deletelist(struct push** rev)
+{
+    struct push *current = *rev;
+    struct push *next = NULL;
+
+    while (current != NULL)
+    {
+        next = current->Ptr;
+        free(current);
+        current = next;
+    }
+    *rev = NULL;
 }
